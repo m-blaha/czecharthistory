3a265eaae4d44296f537e5ccc440ac8f420fe339
Marek Blaha <mb+klokan@dat.cz>
2017-05-04 10:31:53 +0200

    skriptik pro konverzi csv->json

---
+++ data/mkjson.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import csv
+import json
+import sys
+
+
+def mkjson(csvfname):
+    csv_in = csv.reader(file(csvfname, 'r'))
+    fields = csv_in.next()
+    lines = []
+    for line in csv_in:
+        line_d = dict(zip(fields, line))
+        if not line_d['end']: line_d['end'] = line_d['start']
+        line_d['start'] = '01/01/%s' % line_d['start']
+        line_d['end'] = '31/01/%s' % line_d['end']
+        lines.append(line_d)
+    json.dump(lines, file('%s.js' % (csvfname.split('.')[0]), 'w'))
+
+
+if __name__ == '__main__':
+
+    try:
+        csvfname = sys.argv[1]
+    except:
+        print 'nejni csv'
+        sys.exit(2)
+    mkjson(csvfname)
