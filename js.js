a8a13c97de25a4c43ade5980b2ba27c4aaddcf33
Marek Blaha <mb+klokan@dat.cz>
2017-05-05 09:39:55 +0200

    promazani, uprava vypoctu paralax scroll

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
-function TimeLine(startDate)
+function TimeLine(startDate, endDate)
 {
 	var _startDate = startDate
-	var _endDate = new Date();
-	_endDate.setTime(_endDate.getTime() + 150*24*60*60*1000);
+	var _endDate = endDate;
 
 	this.getLength = function(endDate)
 	{
@@ -24,11 +23,6 @@ function TimeLine(startDate)
 	{
 		return Math.floor((second-first)/(1000*60*60*24));
 	}
-}
-
-function recordScrollPosition()
-{
-    _lastScrollPosition = window.pageYOffset;
 }
 
 
@@ -55,61 +49,10 @@ function formatDate(date)
 }
 
 
-function addEvent(type, date, text)
+function addEvent(type, startDate, endDate, text)
 {
     var div = document.createElement('div');
-    div.innerHTML = "<p><b>" + date + "</b><br>" + text + "</p>";
-
-    div.setAttribute('class', 'timeLineEvent');
-
-    div.style.top = _timeline.getDateDistance(parseDate(date)) * DAY_PIXEL_LENGTH + "px";
-    div.style.backgroundColor = getRandomColor();
-
-    var container;
-
-    switch (type)
-    {
-        case EVENT_TYPE.LIFE:
-            container = document.getElementById("divLeftEventContainer");
-            break;
-        case EVENT_TYPE.CAREER:
-            container = document.getElementById("divRightEventContainer");
-            break;
-        default:
-            break;
-    }
-
-    var overlap = false;
-/*		var element;
-
-    for (var i = 0; i < container.children.length; i++)
-    {
-        if (element.style.pixelWidth)
-        {
-            return element.style.pixelWidth;
-        }
-    }
-*/
-    switch (type)
-    {
-        case EVENT_TYPE.LIFE:
-            div.style.right = "10px";
-            break;
-        case EVENT_TYPE.CAREER:
-            div.style.left = "10px";
-            break;
-        default:
-            break;
-    }
-
-    container.appendChild(div);
-}
-
-
-function addEventRange(type, startDate, endDate, text)
-{
-    var div = document.createElement('div');
-    div.innerHTML = "<p><b>" + startDate + " - " + endDate + "</b><br>" + text + "</p>";
+    div.innerHTML = "<div class=\"inner\"><p><b>" + startDate + " - " + endDate + "</b><br>" + text + "</p></div>";
 
     div.setAttribute('class', 'timeLineEvent');
 
@@ -134,47 +77,9 @@ function addEventRange(type, startDate, endDate, text)
     }
 
     container.appendChild(div);
+
+    return div;
 }
-
-
-function addEmptyEvent(maxLength)
-	{
-		var div = document.createElement('div');
-
-		div.setAttribute('class', 'event');
-
-		var width = randomInteger(10, 100);
-		var height = randomInteger(10, width);
-
-		var top = randomInteger(0, maxLength) * DAY_PIXEL_LENGTH;
-
-		div.style.top = top + "px";
-//		div.style.width = width + "px";
-		div.style.height = height + "px";
-		div.style.backgroundColor = getRandomColor();
-		div.style.opacity = 0.5;
-
-		var x_position = randomFloat(0, _containerWidth - width);
-
-		var container;
-
-		if (randomBool())
-		{
-			div.style.right = x_position + "px";
-			container = document.getElementById("divLeftEventContainer");
-		}
-		else
-		{
-			div.style.left = x_position + "px";
-			container = document.getElementById("divRightEventContainer");
-		}
-
-		container.appendChild(div);
-
-		div.scrollSpeed = randomFloat(-1.5, 1.5);
-
-		return div;
-	}
 
 
 function addYear(year)
@@ -284,47 +189,60 @@ function getElementWidth(element)
 }
 
 
-function getElementHeight(element)
-{
-	if (typeof element.clip !== "undefined")
-	{
-		return element.clip.height;
-	}
-	else
-	{
-		if (element.style.pixelHeight)
-		{
-			return element.style.pixelHeight;
-		}
-		else
-		{
-			return element.offsetHeight;
-		}
-	}
+function getPosition(element) {
+    var yPosition = 0;
+
+    while(element) {
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return yPosition;
 }
 
-function scroll()
-	{
-		var change = window.pageYOffset - _lastScrollPosition;
+function relative_scroll(parent) {
+    var ep=$(parent);
+    var ei=$('.inner', ep);
 
-		for (var i = 0; i < _emptyArray.length; i++)
-		{
-			var top = _emptyArray[i].style.top;
+    var ST=$(window).scrollTop();
+    var Sh=$(window).height();
+    var PT=getPosition(parent);
+    var Ph=ep.height();
+    var Eh=ei.height();
 
-			var new_top = parseInt(top.substring(0, (top.length - 2))) + (change * _emptyArray[i].scrollSpeed);
+    var scroll = 0;
+    var progress = 0;
 
-			_emptyArray[i].style.top = new_top + "px";
-		}
+    if (ST < PT-Sh) {
+        scroll = 0;
+    } else if (ST > PT+Ph) {
+        scroll = Ph - Eh;
+    } else {
+        progress = (ST-(PT-Sh))/(Ph+Sh);
+        scroll = Math.round((Ph-Eh)*progress);
+    }
 
-		recordScrollPosition();
-	}
+    ei.css("margin-top", scroll + "px");
+}
+
+function scroll() {
+    for (var i = 0; i < _emptyArray.length; i++) {
+        var parent=_emptyArray[i];
+        relative_scroll(parent);
+    }
+}
 
 
 
 
 
 
-var TIME_START = "01/01/1982";
+
+
+
+
+var TIME_START = "01/01/1970";
+var TIME_END = "01/01/2017";
 var DAY_PIXEL_LENGTH = 0.5
 
 var EVENT_TYPE = {	LIFE:0,
@@ -336,30 +254,18 @@ var _timelineWidth;
 var _emptyArray;
 var _lastScrollPosition;
 
-var eventData = [
-    {"title": "Born", "date": "25/01/1982", "type": 0},
-    {"title": "Primary School", "date": "01/09/1986", "type": 1, "endDate": "30/06/1993"},
-    {"title": "Secondary School", "date": "01/09/1993", "type": 1, "endDate": "30/06/2000"},
-    {"title": "Passed my Driving Test", "date": "22/09/1999", "type": 0},
-    {"title": "University", "date": "01/09/2000", "type": 1, "endDate": "31/05/2004"},
-    {"title": "First job: Software Developer", "date": "04/10/2004", "type": 1, "endDate": "03/08/2007"},
-    {"title": "Second job: Programmer", "date": "07/08/2007", "type": 1, "endDate": "19/12/2007"},
-    {"title": "Third job: Software Developer", "date": "01/01/2008", "type": 1, "endDate": "30/04/2012"},
-    {"title": "Got Married", "date": "09/07/2008", "type": 0},
-    {"title": "First child born", "date": "23/02/2010", "type": 0},
-    {"title": "Joined Twitter", "date": "16/09/2011", "type": 0},
-    {"title": "Fourth job: Senior Software Engineer", "date": "14/05/2012", "type": 1, "endDate": "09/10/2015"},
-    {"title": "Second child born", "date": "03/09/2012", "type": 0}
-];
+eventData = [
+    {"title": "test", "date": "01/01/1985", "endDate": "31/12/2001", "type": 0}
+    ]
+
 
 
 function init()
 	{
-		recordScrollPosition();
-
 		var timeline_start = parseDate(TIME_START);
+		var timeline_end = parseDate(TIME_END);
 
-		_timeline = new TimeLine(timeline_start);
+		_timeline = new TimeLine(timeline_start, timeline_end);
 		var length = _timeline.getLength();
 
 		var right_container = document.getElementById("divRightEventContainer");
@@ -368,54 +274,13 @@ function init()
 		var timeline_container = document.getElementById("divTimeLine");
 		_timelineWidth = getElementWidth(timeline_container);
 
-		// add random scrolling empty events
-		var total_empties = randomInteger(30, 100);
-
+        total_empties = 0;
 		_emptyArray = new Array(total_empties);
 
-		// add to two planes, some before actual events and some after
-		var total_back = randomInteger(0, total_empties);
-		var total_fore = total_empties - total_back;
-
-		// add background
-		for (var i = 0; i < total_back; ++i)
-		{
-			_emptyArray[i] = addEmptyEvent(length);
-		}
-
-		var now = new Date();
 		// add events
 		for (var i = 0; i < eventData.length; i++)
 		{
-			if (typeof(eventData[i].endDate) == "undefined")
-			{
-				addEvent(eventData[i].type, eventData[i].date, eventData[i].title);
-			}
-			else
-			{
-				addEventRange(eventData[i].type, eventData[i].date, eventData[i].endDate, eventData[i].title);
-			}
-		}
-
-		// add an up-to-the-present event
-		// addEventRange(EVENT_TYPE.CAREER, "14/05/2012", now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear(), "Fourth job: Senior Software Engineer");
-
-		var year = now.getFullYear();
-
-		if ((now.getMonth() == 0) && (now.getDate() < 25))
-		{
-			--year;
-		}
-
-		// Add my most recent birthday, a dynamic event
-		addEvent(EVENT_TYPE.LIFE, "25/01/" + year, "My most recent birthday, turned " +  (year - 1982));
-
-		addEvent(EVENT_TYPE.LIFE, formatDate(new Date()), "Today");
-
-		// add foreground
-		for (var i = 0; i < total_fore; ++i)
-		{
-			_emptyArray[total_back + i] = addEmptyEvent(length);
+            _emptyArray[i] = addEvent(eventData[i].type, eventData[i].date, eventData[i].endDate, eventData[i].title);
 		}
 
 		// resize container
@@ -428,7 +293,6 @@ function init()
 
 		while(timeline_line.getFullYear() <= _timeline.endDate().getFullYear())
 		{
-//			timeline_line.setYear(timeline_line.getYear() + 1);
 			addYear(timeline_line.getFullYear())
 			timeline_line.setFullYear(timeline_line.getFullYear() + 1);
 		}
